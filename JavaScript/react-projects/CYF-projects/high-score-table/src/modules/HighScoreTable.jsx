f6ab15f8dc7ab5c4b3256d67889c30f5310c5e8f
Katie <professionalkateg@gmail.com>
2021-11-18 19:30:05 +0000

    completed challenge 2

--- JavaScript/react-projects/CYF-projects/high-score-table/src/modules/HighScoreTable.jsx
+++ JavaScript/react-projects/CYF-projects/high-score-table/src/modules/HighScoreTable.jsx
@@ -8,18 +8,23 @@ const HighScoreTable = (props) => {
         High Scores per Country
       </h1>
       <div id="country-container">
-        {props.scores.map((country) => (
-          <div
-            key={country.name + "-container"}
-            className="card p-3 my-5 border-secondary border-3"
-          >
-            <h2 key={country.name + "-header"} className="m-auto pb-4">
-              <span className="text-uppercase">High Scores:</span>{" "}
-              {country.name}
-            </h2>
-            <PlayerScore scores={country.scores} />
-          </div>
-        ))}
+        {/* sorts the countries then maps that array to different elements */}
+        {props.scores
+          .sort((first, second) =>
+            first.name > second.name ? 1 : second.name > first.name ? -1 : 0
+          )
+          .map((country) => (
+            <div
+              key={country.name + "-container"}
+              className="card p-3 my-5 border-secondary border-3"
+            >
+              <h2 key={country.name + "-header"} className="m-auto pb-4">
+                <span className="text-uppercase">High Scores:</span>{" "}
+                {country.name}
+              </h2>
+              <PlayerScore scores={country.scores} />
+            </div>
+          ))}
       </div>
     </div>
   );
