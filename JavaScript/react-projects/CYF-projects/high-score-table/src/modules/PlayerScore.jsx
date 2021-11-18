24d2ad5acd1a556ae85060303559205972ff199a
Katie <professionalkateg@gmail.com>
2021-11-18 19:33:22 +0000

    completed level 3

--- JavaScript/react-projects/CYF-projects/high-score-table/src/modules/PlayerScore.jsx
+++ JavaScript/react-projects/CYF-projects/high-score-table/src/modules/PlayerScore.jsx
@@ -3,16 +3,20 @@ import React from "react";
 const PlayerScore = (props) => {
   return (
     <div id="score-container">
-      {props.scores.map((score) => (
-        <div
-          key={score.n + "'s-container"}
-          className="p-3 mt-3 border border-info border-5"
-        >
-          <h3 key={score.n + "'s-score"} className="text-center">
-            "{score.n}" : <span className="text-primary">{score.s}</span>
-          </h3>
-        </div>
-      ))}
+      {props.scores
+        .sort((first, second) =>
+          first.s < second.s ? 1 : second.s < first.s ? -1 : 0
+        )
+        .map((score) => (
+          <div
+            key={score.n + "'s-container"}
+            className="p-3 mt-3 border border-info border-5"
+          >
+            <h3 key={score.n + "'s-score"} className="text-center">
+              "{score.n}" : <span className="text-primary">{score.s}</span>
+            </h3>
+          </div>
+        ))}
     </div>
   );
 };
