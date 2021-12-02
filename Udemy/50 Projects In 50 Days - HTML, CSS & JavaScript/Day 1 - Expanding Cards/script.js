f389a92100e604ad75b5e9157dbf37f4411f9cd4
Katie <professionalkateg@gmail.com>
2021-12-02 15:17:26 +0000

    updated the first project to fetch random images from the NASA api

--- Udemy/50 Projects In 50 Days - HTML, CSS & JavaScript/Day 1 - Expanding Cards/script.js
+++ Udemy/50 Projects In 50 Days - HTML, CSS & JavaScript/Day 1 - Expanding Cards/script.js
@@ -1,15 +1,55 @@
-const removeActiveClasses = (panels) =>
+// hiding the API key
+import { API_KEY } from "./API_KEY.js";
+
+// fetching from the NASA api
+const fetchImages = function () {
+  fetch(`https://api.nasa.gov/planetary/apod?count=5&api_key=${API_KEY}`)
+    .then((response) => {
+      console.log("NASA's API was called");
+      return response.json();
+    })
+    .then((data) =>
+      data.forEach((index) =>
+        createPanels({ imageURL: index.url, title: index.title })
+      )
+    )
+    .catch((error) => console.log(error));
+};
+
+// remove the active class onClick event
+const removeActiveClass = (panels) =>
   panels.forEach((panel) => panel.classList.remove("active"));
 
-const expandingPanels = function () {
+// adding a click event to each of the panel div's
+const panelClickEvent = function () {
   const panels = document.querySelectorAll(".panel"); // return a NodeList of all the panels
 
   panels.forEach((panel) => {
     panel.addEventListener("click", () => {
-      removeActiveClasses(panels);
+      removeActiveClass(panels);
       panel.classList.add("active");
     });
   });
 };
 
-window.onload = () => expandingPanels();
+// function called by the fetch to construct the elements and add them to the DOM
+const createPanels = function ({ imageURL, title }) {
+  // making elements and adding them to the DOM
+  const parentContainer = document.querySelector("#main-content");
+  const newDiv = parentContainer.appendChild(document.createElement("div"));
+  const newHeader = newDiv.appendChild(document.createElement("h2"));
+
+  // adding the panel class to our new div
+  newDiv.classList.add("panel");
+
+  // setting the background-image property to be that of the NASA URL
+  newDiv.style.backgroundImage = `url(
+    "${imageURL}"
+  )`;
+  // setting the header text to be that of the NASA title
+  newHeader.innerText = title;
+
+  panelClickEvent();
+};
+
+window.onload = () => fetchImages();
