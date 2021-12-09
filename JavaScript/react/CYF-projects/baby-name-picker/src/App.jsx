e189f518dc338b2eb8f5d0a765f46ad478fbdb6d
Katie <professionalkateg@gmail.com>
2021-12-09 15:30:55 +0000

    added some files, not currently working. Only really commiting so I can compare the fix to this very broken version

---
+++ JavaScript/react/CYF-projects/baby-name-picker/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Testing from "./components/Testing";
+
+const App = () => {
+  return <Testing />;
+};
+
+// renders react
+ReactDOM.render(<App />, document.getElementById("main"));
---
+++ JavaScript/react/CYF-projects/baby-name-picker/src/components/Testing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Testing = () => {
+  return <h3>Screen Show Time, yes yes</h3>;
+};
+
+export default Testing;
