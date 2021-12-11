8c0238f719072cceea9c4eb8fa13ef2f4c6826b8
Katie <professionalkateg@gmail.com>
2021-12-11 09:49:02 +0000

    removed some unused code in the BabyNames script

--- JavaScript/react/CYF-projects/baby-name-picker/JSX/BabyName.js
+++ JavaScript/react/CYF-projects/baby-name-picker/JSX/BabyName.js
@@ -1,13 +1,10 @@
 const BabyName = ({ baby }) => {
-  const [shouldHide, setShouldHide] = React.useState(false);
-
   return (
     <li
       id={baby.id + "-" + baby.name}
       className={`baby-list-item card text-black ${
         baby.sex === "m" ? "baby-blue" : "baby-pink"
       }`}
-      style={shouldHide ? { display: "none" } : {}}
     >
       {baby.name}
     </li>
